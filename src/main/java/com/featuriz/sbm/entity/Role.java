41d53e791a6c38afc24ce9e741f2d4917ce8b60b
sudhakar <featuriz@gmail.com>
2021-11-24 20:22:35 +0530

    Initial Commit

---
+++ src/main/java/com/featuriz/sbm/entity/Role.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.featuriz.sbm.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+/**
+ * @author Sudhakar Krishnan <featuriz@gmail.com>
+ * @Copyright 2009 - 2021 Featuriz
+ * @DateTime 24-Nov-20217:56:21 pm
+ */
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "role_id")
+	private int id;
+	@Column(name = "role")
+	private String role;
+}
---
+++ src/test/java/com/featuriz/sbm/SpringBootMvcDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.featuriz.sbm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest
+class SpringBootMvcDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
